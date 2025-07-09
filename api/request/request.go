178752ac0abd62e0cb2ebd93f940562d6d119c81
antonkolesnikov <girrcen666@icloud.com>
2025-07-09 11:25:06 +0300

    Init

---
+++ api/request/request.go
@@ -0,0 +1,5 @@
+package request
+
+type Response struct {
+	Content []byte `json:"content"`
+}
---
+++ api/response/response.go
@@ -0,0 +1,26 @@
+package response
+
+type Response struct {
+	Status  string `json:"status"`
+	Content []byte `json:"content"`
+	Error   string `json:"error,omitempty"`
+}
+
+const (
+	StatusOK    = "OK"
+	StatusError = "Error"
+)
+
+func Error(msg string) Response {
+	return Response{
+		Status: StatusError,
+		Error:  msg,
+	}
+}
+
+func OK(content []byte) Response {
+	return Response{
+		Status:  StatusOK,
+		Content: content,
+	}
+}
