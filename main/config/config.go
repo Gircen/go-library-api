b28d413e31e13e3152f26f51258f1b624f9daf48
antonkolesnikov <girrcen666@icloud.com>
2025-12-16 13:22:17 +0300

    update Config struct add services array

--- main/config/config.go
+++ main/config/config.go
@@ -5,7 +5,10 @@ type Config struct {
 		Name string `yaml:"name"`
 	} `yaml:"app"`
 	Log struct {
-		Level string `yaml:"level"`
+		Level         string `yaml:"level"`
+		FileMaxSizeMB int    `yaml:"fileMaxSizeMB"`
+		//if count log files > countFilesArchive then program make files to zip archive {nawDate.zip}
+		CountFilesArchive int `yaml:"countFilesArchive"`
 	} `yaml:"log"`
 	Server struct {
 		Name string `yaml:"name"`
@@ -15,9 +18,14 @@ type Config struct {
 	HealthServer struct {
 		Host string `yaml:"host"`
 		Port string `yaml:"port"`
-	} `yaml:"server"`
+	} `yaml:"healthServer"`
 	SettingsServer struct {
 		Host string `yaml:"host"`
 		Port string `yaml:"port"`
 	} `yaml:"settingsServer"`
+	Services []struct {
+		Name string `yaml:"name"`
+		Host string `yaml:"host"`
+		Port string `yaml:"port"`
+	} `yaml:"services"`
 }
